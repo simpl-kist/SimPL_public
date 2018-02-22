d9759480a9b428c7aef4feb29b2c1e7e050e0888
root <root@ip-172-31-13-90.ap-northeast-2.compute.internal>
2018-02-22 02:01:16 +0000

    max_buffer_size

--- config/database.php
+++ config/database.php
@@ -52,6 +52,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+'options' => array( PDO::MYSQL_ATTR_MAX_BUFFER_SIZE => 16777216 ),
         ],
 
         'pgsql' => [
