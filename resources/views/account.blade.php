26a154d3776b6d593be9cb7dcbef655cf6ab20d9
Your Name <you@example.com>
2020-05-18 16:38:54 +0900

    account information

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -67,7 +67,6 @@
 			</tr>
 			<tr>
 				<td colspan=3 style="text-align:right;">
-					<button class="btn btn-outline-primary" onclick="location.href='/';">Home</button>
 					<button class="btn btn-outline-success" onclick="updateMe();">Update</button>
 				</td>
 			</tr>
